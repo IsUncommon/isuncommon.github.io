0ff0da5af28a984fbfc9c01a53bf353e228d6b76
Arpan Chinta <arpancj@gmail.com>
2016-03-09 20:31:00 +0530

    Configure middleman deploy

--- config.rb
+++ config.rb
@@ -37,10 +37,16 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
 end
 
 activate :deploy do |deploy|
